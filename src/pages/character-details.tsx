1da8634d598b3aa1895f0672b4aeda553a2e9a30
prathibha <prathibha@LAPTOP-NAAMUCTV>
2024-01-21 17:40:43 +0530

    details page ui update

--- src/pages/character-details.tsx
+++ src/pages/character-details.tsx
@@ -1,10 +1,11 @@
 import { useEffect } from 'react';
-import { useParams } from 'react-router-dom';
+import { useNavigate, useParams } from 'react-router-dom';
 import { fetchCharacterDetails } from '../context/character-details/actions';
 import { useCharacterDetailsDispatch, useCharacterDetailsState } from '../context/character-details/context';
 
 const CharacterDetails = () => {
   const { id } = useParams();
+  const navigate = useNavigate();
   const characterDetailsDispatch = useCharacterDetailsDispatch();
   const { characterDetails, isLoading, isError, errorMessage } = useCharacterDetailsState();
 
@@ -44,6 +45,9 @@ const CharacterDetails = () => {
   if (!characterDetails) {
     return <div className="text-center mt-8">No character details available</div>;
   }
+  const handleBackButtonClick = () => {
+    navigate('/character'); 
+  };
 
   return (
     <div className="container mx-auto p-4 sm:p-8">
@@ -52,20 +56,41 @@ const CharacterDetails = () => {
         <div className="px-4">
         <img className="rounded-lg mb-4  max-h-48 " src={characterDetails.image} alt={characterDetails.name} />
         </div>
-        <div className="px-4 mb-4">
-          <p>ID: {characterDetails.id}</p>
-          <p>Status: {characterDetails.status}</p>
-          <p>Species: {characterDetails.species}</p>
-          <p>Gender: {characterDetails.gender}</p>
-          <p>Location: {characterDetails.location.name}</p>
-          <p>Origin: {characterDetails.origin.name}</p>
-          <p>Type: {characterDetails.type}</p>
-          <p>URL: {characterDetails.url}</p>
+        <div className="px-4 mb-6">
+          <p className="text-lg font-semibold">Character Information:</p>
+          <p className="mb-2">
+            <span className="font-bold">ID:</span> {characterDetails.id}
+          </p>
+          <p className="mb-2">
+            <span className="font-bold">Status:</span> {characterDetails.status}
+          </p>
+          <p className="mb-2">
+            <span className="font-bold">Species:</span> {characterDetails.species}
+          </p>
+          <p className="mb-2">
+            <span className="font-bold">Gender:</span> {characterDetails.gender}
+          </p>
+          <p className="mb-2">
+            <span className="font-bold">Location:</span> {characterDetails.location.name}
+          </p>
+          <p className="mb-2">
+            <span className="font-bold">Origin:</span> {characterDetails.origin.name}
+          </p>
+          <p className="mb-2">
+            <span className="font-bold">Type:</span> {characterDetails.type || 'N/A'}
+          </p>
+          <p className="mb-2">
+            <span className="font-bold">URL:</span> {characterDetails.url}
+          </p>
         </div>
-        <div className="px-4">
-          <p>Created: {formatDate(characterDetails.created)}</p>
-          <p>
-            Episode:{' '}
+
+        <div className="px-4 mb-6">
+          <p className="text-lg font-semibold">Additional Details:</p>
+          <p className="mb-2">
+            <span className="font-bold">Created:</span> {formatDate(characterDetails.created)}
+          </p>
+          <p className="mb-2">
+            <span className="font-bold">Episodes:</span>{' '}
             {characterDetails.episode.map((episodeLink, index) => (
               <a
                 key={index}
@@ -79,6 +104,15 @@ const CharacterDetails = () => {
             ))}
           </p>
         </div>
+
+        <div className="px-4 mt-4"></div>
+        <button
+            className="bg-blue-500 text-white px-4 py-2 pd-2 ml-4 rounded"
+            onClick={handleBackButtonClick}
+          >
+            Back to characters
+          </button>
+          <div className="mb-4"></div>
       </div>
     </div>
   );
